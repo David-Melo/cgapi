bf8e6f2e32dd7927f49d4cb5d42f855a3b7c6cd5
David Melo <davidmelo@desmdesigns.com>
2020-11-12 15:54:37 -0500

    Docker Init

---
+++ docker/postgres/init/01_init_api_user.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+POSTGRES_API_USER=${POSTGRES_API_USER:-pgapiuser}
+POSTGRES_API_PASS=${POSTGRES_API_PASS:-pgapiuserpass}
+POSTGRES_DB=${POSTGRES_DB:-default}
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" -v user="$POSTGRES_API_USER" -v pass="$POSTGRES_API_PASS" -v db="$POSTGRES_DB" <<-EOSQL
+    CREATE USER :user WITH ENCRYPTED PASSWORD :'pass';
+    GRANT ALL PRIVILEGES ON DATABASE :db TO :user;
+EOSQL
