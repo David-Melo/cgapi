2e7f7b34b0b7f147034044eda1953ff7c96c2f9f
David Melo <davidmelo@desmdesigns.com>
2020-11-30 22:05:57 -0500

    Prod Fixes

--- docker/node/run.sh
+++ docker/node/run.sh
@@ -4,7 +4,7 @@ SCRIPT="$1"
 
 cd $PROJECT_PATH
 
-su -c "cd $PROJECT_PATH; yarn" -s /bin/bash www-app
+su -c "cd $PROJECT_PATH; yarn --production=false" -s /bin/bash www-app
 
 chown -R www-app:www-app /opt/app
 
