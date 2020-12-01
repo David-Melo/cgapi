ba4e3c712ed7f41c3ddcfc7830aeb0f24876292f
David Melo <davidmelo@desmdesigns.com>
2020-11-30 21:41:35 -0500

    Tweaks Deploy Script

--- docker/deploy.sh
+++ docker/deploy.sh
@@ -4,11 +4,11 @@
 ####################################################
 ## Change Working Directory
 ####################################################
-cd /opt/cgapi/docker
+cd /opt/api/docker
 
 ####################################################
 ## Run Docker Commands
 ####################################################
-docker-compose -p cgapi -f docker-compose-prod.yml build
+docker-compose -p cgapi -f docker-compose.prod.yml build
 docker-compose -f docker-compose.prod.yml down
 docker-compose -p cgapi -f docker-compose.prod.yml up -d
