26324bf7c7b37db300952c736d0d8772d85b4b8e
David Melo <davidmelo@desmdesigns.com>
2020-12-29 15:10:24 -0500

    Adds Strapi CMS

---
+++ cms/config/server.js
@@ -0,0 +1,10 @@
+module.exports = ({ env }) => ({
+  host: env('STRAPI_HOST', '0.0.0.0'),
+  port: env.int('STRAPI_PORT', 1337),
+  url: env.int('STRAPI_URL', 'http://localhost'),
+  admin: {
+    auth: {
+      secret: env('STRAPI_JWT_SECRET', null),
+    },
+  },
+});
---
+++ cms/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '5ad2bcad-f138-4366-a7d1-8471b1a853d2'
+};
