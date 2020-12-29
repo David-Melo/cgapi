89b38fdd3e264a78352f31232e72aafca72ff818
David Melo <davidmelo@desmdesigns.com>
2020-12-29 15:35:39 -0500

    Fixes CMS Server Config

--- cms/config/server.js
+++ cms/config/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('STRAPI_HOST', '0.0.0.0'),
   port: env.int('STRAPI_PORT', 1337),
-  url: env.int('STRAPI_URL', 'http://localhost'),
+  url: env('STRAPI_URL', 'http://localhost'),
   proxy: true,
   admin: {
     auth: {
