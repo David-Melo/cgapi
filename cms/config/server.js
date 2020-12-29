01608282ce976e233e40454326759967a993cf89
David Melo <davidmelo@desmdesigns.com>
2020-12-29 15:24:51 -0500

    Enables CMS Proxy

--- cms/config/server.js
+++ cms/config/server.js
@@ -2,6 +2,7 @@ module.exports = ({ env }) => ({
   host: env('STRAPI_HOST', '0.0.0.0'),
   port: env.int('STRAPI_PORT', 1337),
   url: env.int('STRAPI_URL', 'http://localhost'),
+  proxy: true,
   admin: {
     auth: {
       secret: env('STRAPI_JWT_SECRET', null),
